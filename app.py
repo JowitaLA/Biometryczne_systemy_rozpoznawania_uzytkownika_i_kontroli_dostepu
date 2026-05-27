7e7bacda8dbb158756bfa9cefe8befb069bf5e5d
Jowi <JowitaLA01@gmail.com>
2026-05-27 02:48:49 +0200

    test zadania 6

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 import joblib
 import numpy as np
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Wczytanie modelu i skalera
 model = joblib.load("model.pkl")
