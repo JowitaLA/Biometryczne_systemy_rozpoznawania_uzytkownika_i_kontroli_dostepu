684e5b77236b2965ab085bf0d5cc4754b9b2cff1
Jowi <JowitaLA01@gmail.com>
2026-05-27 02:21:44 +0200

    zmiana w zad 6

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+# Wczytanie modelu i skalera
+model = joblib.load("model.pkl")
+scaler = joblib.load("skaler.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+
+    budget = data['budget']
+    popularity = data['popularity']
+    vote_average = data['vote_average']
+    vote_count = data['vote_count']
+
+    budget_log = np.log1p(budget)
+    popularity_log = np.log1p(popularity)
+
+    features = np.array([[
+        budget,
+        popularity,
+        vote_average,
+        vote_count,
+        budget_log,
+        popularity_log
+    ]])
+
+    features = scaler.transform(features)
+    pred = model.predict(features)
+
+    return jsonify({
+        "prediction": float(np.expm1(pred[0]))
+    })
+
+# URUCHOMIENIE NA RENDER
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
